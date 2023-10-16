c2ffeef12dfc18a5c9370cbce39aba1f542e35e0
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2023-10-16 12:24:52 +0200

    UPD

---
+++ CoreApp/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace CoreApp;
+
+public class Class1
+{
+    public void Test()
+    {
+        Console.WriteLine("");
+    }
+
+    public async Task TestAsync()
+    {
+        await using IAsyncDisposable asyncDisposableNumberOne = null,
+            asyncDisposableNumberTwo = null;
+        await Task.Delay(10);
+        Console.WriteLine(asyncDisposableNumberOne);
+        Console.WriteLine(asyncDisposableNumberTwo);
+        Console.WriteLine("Hello, World!");
+    }
+}
